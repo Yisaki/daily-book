02f9a0a7f504ac1a4d6b2d715aab47f4cdc01576
chaos <chaosforhelena@163.com>
2020-12-22 00:32:06 +0800

    抽取组件

---
+++ go/controller/PetController.go
@@ -0,0 +1,59 @@
+package controller
+
+import (
+	"daily/go/logger"
+	"daily/go/model"
+	"daily/go/periodService"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
+func init() {
+
+}
+
+func SaveRecord(w http.ResponseWriter, r *http.Request) {
+
+	body, _ := ioutil.ReadAll(r.Body)
+	logger.Info.Println("SaveRecord:", string(body))
+	period := model.Period{}
+	json.Unmarshal(body, &period)
+
+	periodService.SaveRecord(period.Type)
+
+	result := model.Result{Success: true}
+	resultB, _ := json.Marshal(result)
+	w.Write(resultB)
+}
+
+func GetLastRecord(w http.ResponseWriter, r *http.Request) {
+	logger.Info.Println("GetLastRecord:")
+	period := periodService.GetLastRecord()
+
+	result := model.Result{Success: true, Obj: period}
+	resultB, _ := json.Marshal(result)
+	w.Write(resultB)
+}
+
+func ListRecord(w http.ResponseWriter, r *http.Request) {
+	body, _ := ioutil.ReadAll(r.Body)
+	logger.Info.Println("ListRecord:", string(body))
+
+	page := model.Page{}
+	json.Unmarshal(body, &page)
+
+	if page.Page < 1 {
+		page.Page = 1
+	}
+
+	if page.PageSize < 1 {
+		page.PageSize = 5
+	}
+
+	periods := periodService.ListRecord(page.Page, page.PageSize)
+
+	result := model.Result{Success: true, Obj: periods}
+	resultB, _ := json.Marshal(result)
+	w.Write(resultB)
+}
